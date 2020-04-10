8dd7afe74b15ff3b650664b8bc3d955a4c5879d5
Dellrodar <3357168+Dellrodar@users.noreply.github.com>
2020-04-10 00:36:42 -0400

    added first two listeners.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+const btnSpin = document.querySelectorAll(".btn");
+
+btnSpin.forEach((btnEl) => {
+    btnEl.addEventListener("click", (event) => {
+        event.target.style.transform = "rotate(360deg)";
+        event.target.style.transition = "1s"
+    })
+})
+
+btnSpin.forEach((btnEl) => {
+    btnEl.addEventListener("mouseleave", (event) => {
+        event.target.style.transform = "rotate(-360deg)";
+        event.target.style.transition = "1s"
+    })
+})
+
+const navColor = document.querySelectorAll(".nav-link");
+
+navColor.forEach((navEl) => {
+    navEl.addEventListener("mouseover")
+})
